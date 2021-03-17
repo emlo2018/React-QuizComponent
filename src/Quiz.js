590e22036e23131fc43540d74e80c8c9a4d99741
emlo2018 <emelie_loheden@hotmail.com>
2021-03-17 11:08:06 +0100

    complete logic

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
+//import { stat } from 'fs';
 
 let quizData = require('./quiz_data.json')
 
@@ -7,13 +10,22 @@ class Quiz extends Component {
     super(props)
     this.state = { quiz_position: 1 }
   }
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 }
+    })
+  }
+  handleResetClick() {
+    this.setState({ quiz_position: 1 })
+  }
   render() {
+    const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
     return (
       <div>
-        <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+        {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
       </div>
     )
   }
 }
 
-export default Quiz 
+export default Quiz
